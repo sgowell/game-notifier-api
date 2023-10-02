36a7ca2deabfdbdbca230b9d309b35b63ae8b180
Scott Gowell <sgowell@users.noreply.github.com>
2023-10-02 08:13:04 -0400

    Adding Get Endpoint for stream process function

--- src/twitch/twitch.controller.ts
+++ src/twitch/twitch.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
+import { Logger } from 'nestjs-pino';
+import { TwitchService } from './twitch.service';
 
 @Controller('twitch')
-export class TwitchController {}
+export class TwitchController {
+  constructor(
+    private readonly twitchService: TwitchService,
+    private readonly logger: Logger,
+  ) {}
+
+  @Get()
+  async processStreams(): Promise<void> {
+    await this.twitchService.processStreamsForMessaging();
+  }
+}
